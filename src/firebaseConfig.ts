65084466639bf342c8af3f3849431bef80853688
Giang <minhgiang@adlakeside.com>
2021-03-15 16:06:07 +0700

    mod config

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -1,10 +1,21 @@
+// export const firebaseConfig = {
+//   apiKey: "AIzaSyAyPMuIGreq81gLErYeKGkEje851Xdh_kc",
+//   authDomain: "wavelength-online.firebaseapp.com",
+//   databaseURL: "https://wavelength-online.firebaseio.com",
+//   projectId: "wavelength-online",
+//   storageBucket: "wavelength-online.appspot.com",
+//   messagingSenderId: "811157116454",
+//   appId: "1:811157116454:web:be3d3e6b17629700c038e7",
+//   measurementId: "G-XZ13S15LMC",
+// };
+
 export const firebaseConfig = {
-  apiKey: "AIzaSyAyPMuIGreq81gLErYeKGkEje851Xdh_kc",
-  authDomain: "wavelength-online.firebaseapp.com",
-  databaseURL: "https://wavelength-online.firebaseio.com",
-  projectId: "wavelength-online",
-  storageBucket: "wavelength-online.appspot.com",
-  messagingSenderId: "811157116454",
-  appId: "1:811157116454:web:be3d3e6b17629700c038e7",
-  measurementId: "G-XZ13S15LMC",
+  apiKey: "AIzaSyD1gmzYs_fUyLsIHV_auTGlZha_UlD6Q70",
+  authDomain: "wavelength-xpoko.firebaseapp.com",
+  databaseURL: "https://wavelength-xpoko-default-rtdb.firebaseio.com",
+  projectId: "wavelength-xpoko",
+  storageBucket: "wavelength-xpoko.appspot.com",
+  messagingSenderId: "792537044563",
+  appId: "1:792537044563:web:abaa5e9051ef833e0858a6",
+  measurementId: "G-650DTPN0WX"
 };
